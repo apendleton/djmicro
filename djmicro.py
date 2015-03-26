a7fc2c41823a5d41d9764ff4ed61b4066c16bd89
Andrew Pendleton <apendleton@sunlightfoundation.com>
2015-03-26 16:59:05 -0400

    Refactor settings handling, and add a virtual app for things that require it.

--- djmicro.py
+++ djmicro.py
@@ -1,13 +1,37 @@
-import os, types
+import os, sys, types
 
 _base_module = None
+_app_name = None
 
-def configure(options={}, module=None, local_settings=None):
+def _extend_opts(initial, to_add):
+    if type(to_add) is dict:
+        initial.update(to_add)
+    elif type(to_add) is types.ModuleType:
+        module_opts = {k: v for k, v in to_add.__dict__.iteritems() if not k.startswith('__')}
+        initial.update(module_opts)
+    elif type(to_add) in (tuple, list):
+        for opt_set in to_add:
+            _extend_opts(initial, opt_set)
+    elif to_add is None:
+        pass
+    else:
+        raise ValueError("Options must be a dict, module, list, tuple, or None.")
+
+def configure(options={}, module=None, app_name=None):
     if not module:
         # hack to figure out where we were called from
         import sys, inspect
         module = sys.modules[inspect.stack()[1][0].f_locals['__name__']]
-    
+
+    # djmicro makes an implicit app that you can install models into, add modules from, etc.
+    if not app_name:
+        app_name = "djmicro"
+    global _app_name
+    _app_name = app_name
+
+    if app_name != __name__:
+        sys.modules[app_name] = sys.modules[__name__]
+
     # settings
     from django.conf import settings
     if not settings.configured:
@@ -18,11 +42,10 @@ def configure(options={}, module=None, local_settings=None):
             INSTALLED_APPS = [],
             MIDDLEWARE_CLASSES = ('django.middleware.common.CommonMiddleware',)
         )
-        opts.update(options)
+        _extend_opts(opts, options)
 
-        if local_settings:
-            ls_opts = {k: v for k, v in local_settings.__dict__.iteritems() if not k.startswith('__')}
-            opts.update(ls_opts)
+        if 'djmicro' not in opts['INSTALLED_APPS']:
+            opts['INSTALLED_APPS'] += (app_name,)
 
         settings.configure(**opts)
     
@@ -49,6 +72,26 @@ def route(*args, **kwargs):
         )
         return view
     return add_route
+
+def add_module_to_app(module, name=None):
+    if name is None:
+        name = module.__name__
+
+    # make it available as a module
+    sys.modules["%s.%s" % (_app_name, name)] = module
+    if _app_name != __name__:
+        sys.modules["%s.%s" % (__name__, name)] = module
+
+    # make it available with a from import
+    globals()[name] = module
+
+    # if it's a models module, special-case handle it because model detection has already occurred
+    if name == "models":
+        from django.apps import apps
+        new_config = apps.get_app_config(_app_name)
+        new_config.import_models(apps.all_models[_app_name])
+        apps.app_configs[_app_name] = new_config
+
 
 def run():
     from django.core.management import execute_from_command_line
